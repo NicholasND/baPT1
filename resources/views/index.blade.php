719c81700c3a8fa7425a8bd6c7070ea7a970c0fe
NicholasND <94053312+NicholasND@users.noreply.github.com>
2023-09-19 20:55:24 +0700

    Initial commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,9 @@
+<html>
+    <head></head>
+    <body>
+        <h1>Selamat Datang <br>
+            di Website </h1>
+        <h2>Nicholas Dickson</h2>
+        <a href="profil">ke halaman Profil</a>
+    </body>
+</html>
---
+++ resources/views/profil.blade.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>Profil Saya</title>
+    </head>
+    <body>
+        <h1>Profil Biodata</h1>
+        <h2>Nama : Nicholas Dickson</h2>
+        <h3>NIM : 03081210020</h3>
+        <h4>Kelas : 21SI2</h4>
+        <h5>Jurusan : Sistem Informasi</h5>
+    </body>
+<html>
