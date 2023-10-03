05fd63866286282c6b4bef8bb3703738654c552e
NicholasND <94053312+NicholasND@users.noreply.github.com>
2023-10-03 20:49:26 +0700

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,9 +1,13 @@
-<html>
-    <head></head>
-    <body>
-        <h1>Selamat Datang <br>
-            di Website </h1>
-        <h2>Nicholas Dickson</h2>
-        <a href="profil">ke halaman Profil</a>
-    </body>
+@extends('layouts.app')
+
+@section('title','Home')
+@section('content')
+<head></head>
+<body>
+    <h1>Selamat Datang <br>
+        di Website </h1>
+    <h2>Nicholas Dickson</h2>
+    <a href="profil">ke halaman Profil</a>
+</body>
 </html>
+@endsection
