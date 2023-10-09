c6b54a9961912325f667e3e01c25f1730727e62c
NicholasND <94053312+NicholasND@users.noreply.github.com>
2023-10-09 13:36:49 +0700

    Update khsController.php

--- app/Http/Controllers/khsController.php
+++ app/Http/Controllers/khsController.php
@@ -7,4 +7,44 @@ use Illuminate\Http\Request;
 class khsController extends Controller
 {
     //
+    public function index()
+    {
+        //List index
+    }
+
+    //
+    public function create()
+    {
+        //Create new resource
+    }
+
+    //
+
+    public function store(Request $request)
+    {
+        //Store newly created storage
+    }
+
+    //
+    public function show(string $id)
+    {
+        //Display resource 
+    }
+
+    //
+    public function edit(string $id)
+    {
+        //Show edit form for resource
+    }
+
+    //
+    public function update(Request $request, string $id)
+    {
+        //Update resource
+    }
+
+    public function destroy(string $id)
+    {
+        //
+    }
 }
