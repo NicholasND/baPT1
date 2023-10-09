2948467bf23fa6bf16592934e37794fc8f1f1598
NicholasND <94053312+NicholasND@users.noreply.github.com>
2023-10-09 13:37:02 +0700

    Update mahasiswa.blade.php

--- resources/views/mahasiswa.blade.php
+++ resources/views/mahasiswa.blade.php
@@ -2,90 +2,52 @@
 use App\Models\mahasiswa;
 ?>
 @extends('layouts.app')
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+    <style>
+        th {
+            border: 1px solid;
+            text-align: center;
+            padding: 12px;
+        }
+        td {
+            border: 1px solid;
+            text-align: center;
+            padding: 12px;
+        }
+    </style>
 @section('title','Mahasiswa')
 @section('content')
-<div>
-    <p style="text-align:center; font-size: 48px; margin-top:36px; font-weight: bold">DATA MAHASISWA</p>
-    <div class="container" style="max-width:70%">
-        <div class="row bold-font">
+    <div class= "container">
+        <p style= "font-size: 36px ; font-weight: bold ; text-align: start ; margin: 40px 0 0 0">DATA MAHASISWA</p>     
+        <table style= "margin-top: 16px">
             <tr>
                 <th>ID</th>
                 <th>StudentID</th>
                 <th>Nama</th>
                 <th>Jurusan</th>
+                <th>Tahun Masuk</th>
             </tr>
+
             <tr>
-                <?php
-                    $mahasiswas = mahasiswa::all();
-                    foreach ($mahasiswas as $mahasiswa) 
-                    <tr>
-                        <td>{{ $mahasiswa->id}}</td>
-                        <td>{{ $mahasiswa->studentID}}</td>
-                        <td>{{ $mahasiswa->nama}}</td>
-                        <td>{{ $mahasiswa->jurusan}}</td>
-                    </tr>
-                    @endforeach
-                ?>
-            <tr>
-            <div class="col-3">
-                Nama
-            </div>
-            <div class="col-9">
-                : Nicholas Dickson
-            </div>
-        </div>
-        <div class="row bold-font">
-            <div class="col-3">
-                NIM
-            </div>
-            <div class="col-9">
-                : 03081210020
-            </div>
-        </div>
-        <div class="row bold-font">
-            <div class="col-3">
-                Kelas
-            </div>
-            <div class="col-9">
-                : 21SI2
-            </div>
-        </div>
-        <div class="row bold-font">
-            <div class="col-3">
-                Jurusan
-            </div>
-            <div class="col-9">
-                : Sistem Informasi
-            </div>
-        </div>
+            @foreach ($mahasiswas as $mahasiswa) 
+                <tr>
+                    <td>
+                        {{ $mahasiswa->id }}
+                    </td>
+                    <td>
+                        {{ $mahasiswa->studentID}}
+                    </td>
+                    <td>
+                        {{ $mahasiswa->nama }}
+                    </td>
+                    <td>
+                        {{ $mahasiswa->jurusan }}
+                    </td>
+                    <td>
+                        {{ $mahasiswa->tahunMasuk }}
+                    </td>
+                </tr>
+                @endforeach
+        </table>
     </div>
 @endsection
-<link href ="https//cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stlyesheet">
-    <style>
-        th {
-            border: 1px solid black;
-            padding: 8px;
-            font-weight: bold;
-            text-align: center;
-        }
-        td {
-            border: 1px solid black;
-            padding: 8px;
-            text-align: center;
-        }
-        .bold-font {
-            font-weight: bold;
-        }
-        </style>
-<html>
-    <head>
-        <title>Profil Saya</title>
-    </head>
-    <body>
-        <h1>Profil Biodata</h1>
-        <h2>Nama : Nicholas Dickson</h2>
-        <h3>NIM : 03081210020</h3>
-        <h4>Kelas : 21SI2</h4>
-        <h5>Jurusan : Sistem Informasi</h5>
-    </body>
-<html>
